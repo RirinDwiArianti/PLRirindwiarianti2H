16c720c8ce98555e0be0d458b2e016067fb69c71
RirinDwiArianti <12050120419@students.uin-suska.ac.id>
2021-03-30 11:22:34 +0700

    ifelsestatements

---
+++ Src/ConditionalAndLopps/Nestedifstatements.java
@@ -0,0 +1,14 @@
+package ConditionalAndLopps;
+
+public class Nestedifstatements {
+
+    public static void main(String[] args) {
+        int x = 37;
+        if ( x > 22){
+            if ( x > 31){
+                System.out.print(" it works!");
+            }
+        }
+    }
+}
+
---
+++ Src/ConditionalAndLopps/forLoops.java
@@ -0,0 +1,10 @@
+package ConditionalAndLopps;
+
+public class forLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Great!");
+        }
+    }
+}
+
